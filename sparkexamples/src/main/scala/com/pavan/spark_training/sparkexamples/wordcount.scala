551d18fe619ddb0d2eebf82d4ac804de9452e312
pavan <pavan@centos7-test.c.empirical-realm-200012.internal>
2018-05-05 15:40:40 +0000

    Word Count Program in Spark Scala
    Change-Id: I96e7bb0072a7ef6002994c43aa0d29eb1b2b2df3

---
+++ sparkexamples/src/main/scala/com/pavan/spark_training/sparkexamples/wordcount.scala
@@ -0,0 +1,30 @@
+package com.pavan.spark_training.sparkexamples
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object wordcount {
+  def main(args: Array[String]): Unit = {
+    
+    val conf = new SparkConf()
+    .setAppName("Word Count")
+    .setMaster("local")
+    
+    val sc = new SparkContext(conf)
+    
+    if (args.length < 1) {
+      println("Missing Arguments !!!!!")
+    }
+    
+    val textFile = sc.textFile(args(0))
+    
+    val word_count = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_+_)
+    
+    val sorted = word_count.sortByKey()
+    
+    //sorted.foreach(println)
+    sorted.collect.foreach(println)
+    
+    //sorted.collect.saveAsTextFile(args(1))
+  }
+}
