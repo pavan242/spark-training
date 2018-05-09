95ad190529e9f4bec8d5c5d5770b790bcfd71997
pavan <pavan@centos7-test.c.empirical-realm-200012.internal>
2018-05-09 19:15:03 +0000

    Updated the read function in example 1.

--- sparkexamples/src/main/scala/com/pavan/spark_training/sparkexamples/learningjournal01.scala
+++ sparkexamples/src/main/scala/com/pavan/spark_training/sparkexamples/learningjournal01.scala
@@ -10,22 +10,22 @@ object learningjournal01 {
                        .appName("learning Journal example 01")
                        .getOrCreate()
 
-    val df = sparkSession.read.options( Map("header" -> "true",
+    /*val df = sparkSession.read.options( Map("header" -> "true",
                                      "inferSchema" -> "true", 
                                      "nullValue" -> "NA",
                                      "timestampFormat" -> "yyyy-MM-dd'T'HH:mm?:ss",
                                      "mode" -> "failfast")
-                                ).csv("/home/pavan/Data/survey.csv")
+                                ).csv(args(0))*/
      
-     /*val df = spark.read
+     val df = sparkSession.read
                   .format("csv")
                   .option("header","true")
                   .option("inferSchema","true") 
                   .option("nullValue","NA")
                   .option("timestampFormat","yyyy-MM-dd'T'HH:mm?:ss")
                   .option("mode","failfast")
-                  .option("path","/home/pavan/Data/survey.csv")
-                  .load()*/
+                  .option("path",args(0))
+                  .load()
 
     df.select("Timestamp", "Age","remote_work","leave").filter("Age > 30").show
 
